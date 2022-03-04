d21fe556bd8e3669ba3ba5ee19903984054aa667
Paul Makles <paulmakles@gmail.com>
2022-03-04 19:25:42 +0000

    fix: commit app files

---
+++ app/src/main/java/chat/revolt/app/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package chat.revolt.app.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
